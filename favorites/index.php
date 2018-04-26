955a4cb97f8e1f4c04ba6b1a096ed950d8e701e0
Noah Scholfield <contact@noahscholfield.com>
2018-04-25 21:35:42 -0400

    Add footer

--- favorites/index.php
+++ favorites/index.php
@@ -113,6 +113,10 @@ HERE;
         ?>
       </div> <!-- /.row -->
     </div>
+    <footer>
+      <p class="col-even">Images from <a href="https://unsplash.com/collections/1953059/framed" target="_blank" rel="noopener noreferrer">Unsplash</a></p>
+      <h4 class="col-even">Designed and built by <a href="https://noahscholfield.com/" target="_blank">Noah Scholfield</h4>
+    </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha256-C8oQVJ33cKtnkARnmeWp6SDChkU+u7KvsNMFUzkkUzk=" crossorigin="anonymous"></script>
   </body>
