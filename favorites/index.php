ceafde0cc03b399f14be9aefad3b7f8c45b99f75
Noah Scholfield <contact@noahscholfield.com>
2018-04-25 21:16:13 -0400

    Update Bootstrap to 4.1.0

--- favorites/index.php
+++ favorites/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Favorites | Framed </title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,900" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0/yeti/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.0/yeti/bootstrap.min.css" integrity="sha256-vbJ7pUbGkPDqyJtw7J9dSR0HPU1TURYQ7/DnkRqTjWg=" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/store.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
@@ -26,7 +26,10 @@
             <a class="nav-link" href="/about/">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="/store/">Store</a>
+            <a class="nav-link" href="/store/">Store</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="/favorites/">Favorites</a>
           </li>
         </ul>
         <ul class="navbar-nav">
@@ -49,41 +52,64 @@
     </div>
     <div class="container">
         <?php 
-          $connection = mysqli_connect("127.0.0.1", "njscholf_labs", "CS 334 Labs", "njscholf_cs334");
-          if (!$connection) {
-            echo "Error: Unable to connect to MySQL." . PHP_EOL;
-            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-            exit;
+          if (filter_has_var(INPUT_GET, "username")){
+            displayResults();
+          } else {
+            displayForm();
           }
-          
-          $query = "SELECT products.productID, name, photographer, category, color, imageURL, description FROM products JOIN favorites JOIN users WHERE products.productID = favorites.productID && users.userID = favorites.userID && username = '{$_GET['username']}'";
-          
-          $results = mysqli_query($connection, $query);
-          
-          if($results) {
-            echo '<div class="row align-items-stretch">';
-            while($row = mysqli_fetch_assoc($results)) {
-              print <<<HERE
-              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-3">
-                <div class="card">
-                  <img class="card-img-top" src="{$row['imageURL']}/500x500/" alt="{$row['name']}">
-                  <div class="card-body">
-                    <h5 class="card-title">{$row['name']}</h5>
-                    <h6 class="text-muted">By {$row['photographer']}</h6>
-                    <span class="badge badge-pill badge-success">{$row['category']}</span>
-                    <p class="card-text">{$row['description']}</p>
-                    <a href="/item/?id={$row['productID']}" class="btn btn-primary">Details</a>
+        
+          function displayForm() {
+            print <<<HERE
+            <form action="" method="GET">
+              <fieldset>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input class="form-control" type="text" name="username" required>
+                </div>
+              </fieldset>
+              <div class="form-group">
+                <button class="btn btn-primary">Search</button>
+              </div>
+            </form>
+HERE;
+          }
+        
+          function displayResults() {
+            $connection = mysqli_connect("127.0.0.1", "njscholf_labs", "CS 334 Labs", "njscholf_cs334");
+            if (!$connection) {
+              echo "Error: Unable to connect to MySQL." . PHP_EOL;
+              echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+              echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+              exit;
+            }
+            
+            $query = "SELECT products.productID, name, photographer, category, color, imageURL, description FROM products JOIN favorites JOIN users WHERE products.productID = favorites.productID && users.userID = favorites.userID && username = '{$_GET['username']}'";
+            
+            $results = mysqli_query($connection, $query);
+            
+            if($results) {
+              echo '<div class="row align-items-stretch">';
+              while($row = mysqli_fetch_assoc($results)) {
+                print <<<HERE
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-3">
+                  <div class="card">
+                    <img class="card-img-top" src="{$row['imageURL']}/500x500/" alt="{$row['name']}">
+                    <div class="card-body">
+                      <h5 class="card-title">{$row['name']}</h5>
+                      <h6 class="text-muted">By {$row['photographer']}</h6>
+                      <span class="badge badge-pill badge-success">{$row['category']}</span>
+                      <p class="card-text">{$row['description']}</p>
+                      <a href="/item/?id={$row['productID']}" class="btn btn-primary">Details</a>
+                    </div>
                   </div>
                 </div>
-              </div>
 HERE;
+              }
+            } else {
+              echo '<h4 class="text-primary">No products to show</h4>';
             }
-          } else {
-            echo '<h4 class="text-primary">No products to show</h4>';
+            mysqli_close($connection);
           }
-          mysqli_close($connection);
-          
         ?>
       </div> <!-- /.row -->
     </div>
--- item/index.php
+++ item/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Item Page | Framed </title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,900" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.0.0/yeti/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.0/yeti/bootstrap.min.css" integrity="sha256-vbJ7pUbGkPDqyJtw7J9dSR0HPU1TURYQ7/DnkRqTjWg=" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/item.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
@@ -27,6 +27,9 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/store/">Store</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/favorites/">Favorites</a>
           </li>
         </ul>
         <ul class="navbar-nav">
