45681314e3d4ab328d458c9e7038e293b4df8a76
Noah Scholfield <contact@noahscholfield.com>
2018-11-03 17:21:18 -0400

    Break out reused code into external files

--- add-item/index.php
+++ add-item/index.php
@@ -1,51 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Add New Item | Framed </title>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,900" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.0/yeti/bootstrap.min.css" integrity="sha256-vbJ7pUbGkPDqyJtw7J9dSR0HPU1TURYQ7/DnkRqTjWg=" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/app.css">
+    <?php 
+      $pageTitle = "Add Item";
+      require('../partials/head.php');
+    ?>
     <link rel="stylesheet" href="/css/item.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
   </head>
   <body>
     <div class="f-pusher">
-      <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="/"><span class="far fa-images"></span> Framed</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/about/">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/store/">Store</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/favorites/">Favorites</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#"><span title="Follow us on Instagram!" class="fab fa-instagram"></span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><span title="Follow us on Twitter!" class="fab fa-twitter"></span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><span title="Like us on Facebook!" class="fab fa-facebook-f"></span></a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <?php include('../partials/navbar.php'); ?>
       <div class="jumbotron">
         <div class="container">
           <h1 class="display-4">Add New Item</h1>
@@ -117,13 +81,6 @@ HERE;
         ?>
       </div>
     </div>
-    <footer>
-      <div class="row">
-        <p class="col">Images from <a href="https://unsplash.com/collections/1953059/framed" target="_blank" rel="noopener noreferrer">Unsplash</a></p>
-        <h6 class="col">Designed and built by <a href="https://noahscholfield.com/" target="_blank">Noah Scholfield</h6>
-        </div>
-    </footer>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha256-C8oQVJ33cKtnkARnmeWp6SDChkU+u7KvsNMFUzkkUzk=" crossorigin="anonymous"></script>
+    <?php include('../partials/footer.php'); ?>
   </body>
 </html>
---
+++ partials/head.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+  if($pageTitle == 'Home') {
+    echo "<title>Framed | Framed images for your dorm, apartment, or house</title>";
+  } else {
+    echo "<title>$pageTitle | Framed</title>";
+  }
+?>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,900" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.3/yeti/bootstrap.min.css" integrity="sha256-W2ya1Ygm18l60afsKnKMJEJDN/LjR4hn1kK3ngv/J4o=" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/app.css">
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
