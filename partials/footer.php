3fe28b99c1a9bd7bd339f83de010b6dfe84f0cc0
Noah Scholfield <contact@noahscholfield.com>
2018-11-15 22:48:23 -0500

    Fix footer link

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="row">
     <p class="col">Images from <a href="https://unsplash.com/collections/1953059/framed" target="_blank" rel="noopener noreferrer">Unsplash</a></p>
-    <h6 class="col">Designed and built by <a href="https://noahscholfield.com/" target="_blank">Noah Scholfield</h6>
+    <h6 class="col">Designed and built by <a href="https://noahscholfield.com/" target="_blank">Noah Scholfield</a></h6>
     </div>
 </footer>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
