a4fcc13f9ac3f736e61da38afd39a47fc457870d
Noah Scholfield <contact@noahscholfield.com>
2018-11-28 14:25:08 -0500

    Don't show table header for errors

--- profile/orders/index.php
+++ profile/orders/index.php
@@ -21,44 +21,36 @@
         </div>
       </div>
       <div class="container">
-        <?php require('../../partials/navbar.php'); ?>
-        <table class="table">
-          <tr>
-            <th>Order #</th>
-            <th>Item Name</th>
-            <th>Frame</th>
-            <th>Shipping</th>
-            <th>Status</th>
-            <th>Date Ordered</th>
-          </tr>
-          <?php
-            require('../../partials/database.php');
-            $orderQuery = "SELECT FramedOrders.orderID, FramedProducts.productID, FramedProducts.name, frame, imageURL, shippingMethod, status, timestamp
-                           FROM FramedOrders JOIN FramedOrderItems ON FramedOrders.orderID = FramedOrderItems.orderID
-                                             LEFT JOIN FramedProducts ON FramedProducts.productID = FramedOrderItems.productID
-                           WHERE userID = {$_SESSION['userID']};";
-            $orders = mysqli_query($connection, $orderQuery);
-            if(!$orders) {
-              echo '<h4 class="text-danger">Error loading past orders</h4>';
-            } else if(mysqli_num_rows($orders) == 0) {
-              echo '<h4 class="text-info">Looks like you haven\'t ordered anything yet</h4>';
-            } else {
-              while($row = mysqli_fetch_assoc($orders)) {
-                $dateString = date('M j, Y g:i A', strtotime($row['timestamp']));
-                echo <<<HERE
-                <tr>
-                  <td>{$row['orderID']}</td>
-                  <td><a href="{$_ENV["SERVER_ROOT"]}/item/?id={$row['productID']}">{$row['name']}</a></td>
-                  <td>{$row['frame']}</td>
-                  <td>{$row['shippingMethod']}</td>
-                  <td>{$row['status']}</td>
-                  <td>{$dateString}</td>
-                </tr>
+        <?php
+          require('../../partials/navbar.php');
+          require('../../partials/database.php');
+          $orderQuery = "SELECT FramedOrders.orderID, FramedProducts.productID, FramedProducts.name, frame, imageURL, shippingMethod, status, timestamp
+                         FROM FramedOrders JOIN FramedOrderItems ON FramedOrders.orderID = FramedOrderItems.orderID
+                                           LEFT JOIN FramedProducts ON FramedProducts.productID = FramedOrderItems.productID
+                         WHERE userID = {$_SESSION['userID']};";
+          $orders = mysqli_query($connection, $orderQuery);
+          if(!$orders) {
+            echo '<h4 class="text-danger">Error loading past orders</h4>';
+          } else if(mysqli_num_rows($orders) == 0) {
+            echo '<h4 class="text-info">Looks like you haven\'t ordered anything yet.</h4>';
+          } else {
+            echo '<table class="table"><tr><th>Order #</th><th>Item Name</th><th>Frame</th><th>Shipping</th><th>Status</th><th>Date Ordered</th></tr>';
+            while($row = mysqli_fetch_assoc($orders)) {
+              $dateString = date('M j, Y g:i A', strtotime($row['timestamp']));
+              echo <<<HERE
+              <tr>
+                <td>{$row['orderID']}</td>
+                <td><a href="{$_ENV["SERVER_ROOT"]}/item/?id={$row['productID']}">{$row['name']}</a></td>
+                <td>{$row['frame']}</td>
+                <td>{$row['shippingMethod']}</td>
+                <td>{$row['status']}</td>
+                <td>{$dateString}</td>
+              </tr>
 HERE;
-              }
             }
-          ?>
-        </table>
+            echo "</table>";
+          }
+        ?>
       </div>
     </div>
     <?php include('../../partials/footer.php'); ?>
