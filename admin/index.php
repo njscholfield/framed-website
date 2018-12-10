b1568b0d62001347f4aff17feb7153e1ba689ca9
noah.scholfield <noah.scholfield@gmail.com>
2018-12-10 17:32:40 -0500

    Add pending orders to dashboard

--- admin/index.php
+++ admin/index.php
@@ -20,6 +20,7 @@
       <?php
         include('../partials/navbar.php');
         include('../partials/adminSidebar.php');
+        include('../partials/database.php');
       ?>
       <div class="jumbotron">
         <div class="container">
@@ -27,12 +28,48 @@
         </div>
       </div>
       <div class="container">
-        <h1>Actions</h1>
-        <ul>
-          <li><a href="orders/">View All Orders</a></li>
-          <li><a href="items/">Edit Item Details</a></li>
-          <li><a href="add-item/">Add Item Details</a></li>
-        </ul>
+        <div class="card">
+          <div class="card-header text-white bg-success">
+            Pending Orders
+          </div>
+          <table class="table">
+            <tr>
+              <th>Order #</th>
+              <th>Item Name</th>
+              <th>Frame</th>
+              <th>Shipping</th>
+              <th>Status</th>
+              <th>Date Ordered</th>
+            </tr>
+            <?php
+              $orderQuery = "SELECT FramedOrders.orderID, FramedProducts.productID, FramedProducts.name, frame, imageURL, shippingMethod, status, timestamp
+                             FROM FramedOrders JOIN FramedOrderItems ON FramedOrders.orderID = FramedOrderItems.orderID
+                                          LEFT JOIN FramedProducts ON FramedProducts.productID = FramedOrderItems.productID
+                             WHERE status = 'Processing';";
+              $results = mysqli_query($connection, $orderQuery);
+              if($results) {
+                $last = 0;
+                while($row = mysqli_fetch_assoc($results)) {
+                  $orderID = ($last == $row['orderID']) ? '' : $row['orderID']; // only show the order id for the first item
+                  $dateString = date('M j, Y g:i A', strtotime($row['timestamp']));
+                  echo <<<HERE
+                    <tr>
+                     <td>$orderID</td>
+                     <td><a href="{$_ENV["SERVER_ROOT"]}/item/?id={$row['productID']}">{$row['name']}</a></td>
+                     <td>{$row['frame']}</td>
+                     <td>{$row['shippingMethod']}</td>
+                     <td>{$row['status']}</td>
+                     <td>{$dateString}</td>
+                   </tr>
+HERE;
+                  $last = $row['orderID'];
+                }
+                mysqli_free_result($results);
+              }
+              mysqli_close($connection);
+            ?>
+          </table>
+        </div>
       </div>
     </div>
     <?php include('../partials/footer.php'); ?>
