6f25841228a37295898c014385eadce51c8e7027
Noah Scholfield <contact@noahscholfield.com>
2018-12-11 01:52:59 -0500

    Switch to link instead of details button

--- partials/itemCard.php
+++ partials/itemCard.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
   function printItemCards($results) {
     echo '<div class="row align-items-stretch">';
     while($row = mysqli_fetch_assoc($results)) {
@@ -7,13 +7,12 @@
         <div class="card">
           <img class="card-img-top" src="{$row['imageURL']}/500x500/" alt="{$row['name']}">
           <div class="card-body">
-            <h5 class="card-title">{$row['name']}</h5>
+            <a href="{$_ENV['SERVER_ROOT']}/item/?id={$row['productID']}"><h5 class="card-title">{$row['name']}</h5></a>
             <h6 class="text-muted">By {$row['photographer']}</h6>
             <span class="badge badge-pill badge-success">{$row['category']}</span>
             <p class="card-text">{$row['description']}</p>
           </div>
           <div class="card-footer">
-            <a href="{$_ENV['SERVER_ROOT']}/item/?id={$row['productID']}" class="btn btn-primary">Details</a>
             <button data-item="{$row['productID']}" class="btn btn-success fav-btn"><span class="fas fa-heart"></span> Favorite</button>
           </div>
         </div>
