d3f29576f2f23267cd4ed8dbbcdea41ff630aeb6
Noah Scholfield <contact@noahscholfield.com>
2018-12-11 01:41:04 -0500

    Implement change password

--- profile/index.php
+++ profile/index.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php 
+    <?php
       define("PAGE_TITLE", "Account Settings");
       require('../partials/head.php');
     ?>
@@ -21,31 +21,74 @@
       </div>
     </div>
     <div class="container">
-      <?php 
+      <?php
         require('../partials/database.php');
-        
-        if(!empty($_POST) && isset($_POST['visibility'])) {
-          // Check for valid input before updating visibility
-          if(($_POST['visibility'] == 0 || $_POST['visibility'] == 1)) {
-            $updateVisibility = "UPDATE FramedUsers
-                                 SET publicProfile = {$_POST['visibility']}
-                                 WHERE userID = {$_SESSION['userID']}";
-            $result = mysqli_query($connection, $updateVisibility);
-            if(!result) {
-              echo '<h4 class="text-danger">Error updating favorites visibility</h4>';
+
+        if(!empty($_POST)) {
+          if(isset($_POST['visibility'])) {
+            // Check for valid input before updating visibility
+            if(($_POST['visibility'] == 0 || $_POST['visibility'] == 1)) {
+              $updateVisibility = "UPDATE FramedUsers
+                                   SET publicProfile = {$_POST['visibility']}
+                                   WHERE userID = {$_SESSION['userID']}";
+              $result = mysqli_query($connection, $updateVisibility);
+              if(!$result) {
+                echo '<h4 class="text-danger">Error updating favorites visibility</h4>';
+              }
+            }
+          } else if(isset($_POST['currentPassword'])) {
+            $pwQuery = "SELECT password FROM FramedUsers WHERE userID = {$_SESSION['userID']};";
+            $result = mysqli_query($connection, $pwQuery);
+            $currentPW = mysqli_fetch_assoc($result)['password'];
+
+            if(!password_verify($_POST['currentPassword'], $currentPW)) {
+              $errors['currentPassword'] = "Incorrect current password";
+            } else if(strlen($_POST['newPassword1']) < 8 || strlen($_POST['newPassword1']) > 72) {
+              $errors['newPassword1'] = "Password must be 8-72 characters long";
+            } else if($_POST['newPassword1'] !== $_POST['newPassword2']) {
+              $errors['newPassword2'] = "New passwords don't match";
+            } else {
+              $newPW = password_hash($_POST['newPassword1'], PASSWORD_DEFAULT);
+              $updatePW = "UPDATE FramedUsers
+                           SET password = '{$newPW}'
+                           WHERE userID = {$_SESSION['userID']};";
+              $successful = mysqli_query($connection, $updatePW);
+              if($successful) {
+                echo '<h4 class="text-success">Password changed!</h4>';
+              } else {
+                echo '<h4 class="text-danger">Error changing password!</h4>';
+              }
             }
           }
         }
-        
-        $accountQuery = "SELECT userID, firstName, lastName, username, email, publicProfile, role
-                         FROM FramedUsers
-                         WHERE userID = {$_SESSION['userID']}";
-        $userData = mysqli_query($connection, $accountQuery);
-        if($userData && mysqli_num_rows($userData) == 1) :
-          $data = mysqli_fetch_assoc($userData);
-          foreach ($data as $key => $value) {
-            echo "<p><strong>$key:</strong> $value</p>";
-          }
+        ?>
+        <form action="" method="post">
+          <legend>Change Password</legend>
+          <fieldset>
+            <div class="form-group">
+              <label for="currentPassword">Current Password</label>
+              <?php if(isset($errors['currentPassword'])) echo "<small class=\"text-danger\">{$errors['currentPassword']}</small>";?>
+              <input class="form-control" name="currentPassword" type="password" required="true">
+            </div>
+            <div class="form-group">
+              <label for="newPassword1">New Password</label>
+              <?php if(isset($errors['newPassword1'])) echo "<small class=\"text-danger\">{$errors['newPassword1']}</small>";?>
+              <input class="form-control" name="newPassword1" type="password" required="true" minlength="8" maxlength="72">
+            </div>
+            <div class="form-group">
+              <label for="newPassword2">Confirm New Password</label>
+              <?php if(isset($errors['newPassword2'])) echo "<small class=\"text-danger\">{$errors['newPassword2']}</small>";?>
+              <input class="form-control" name="newPassword2" type="password" required="true" minlength="8" maxlength="72">
+            </div>
+          </fieldset>
+          <div class="form-group">
+            <button class="btn btn-primary" type="submit">Submit</button>
+          </div>
+        </form>
+        <?php
+          $visibilityQuery = "SELECT publicProfile FROM FramedUsers WHERE userID = {$_SESSION['userID']};";
+          $result = mysqli_query($connection, $visibilityQuery);
+          $data = mysqli_fetch_assoc($result);
         ?>
         <form action="" method="post">
           <legend>Change Favorites Visibility</legend>
@@ -63,11 +106,9 @@
         </form>
         <?php $favURL = "/favorites/?user={$_SESSION['username']}"; ?>
         <h6>If your favorites are public, people can see them at <a href="<?php path($favURL); ?>"><?php echo $_SERVER['SERVER_NAME'];
-         path($favURL); ?></a></h5>
-        <?php else : ?>
-          <h4 class="text-danger">Error loading account info</h4>
-        <?php endif;
-          mysqli_free_result($userData);
+         path($favURL); ?></a></h6>
+         <?php
+          mysqli_free_result($result);
           mysqli_close($connection);
         ?>
       <a class="btn btn-danger" href="<?php path('/logout/'); ?>">Log Out</a>
