c2be8a2662a20b4d1463c11a8f9bae277b72d432
Noah Scholfield <contact@noahscholfield.com>
2018-12-11 00:52:23 -0500

    Add contact form and customer name to order

--- admin/index.php
+++ admin/index.php
@@ -36,15 +36,17 @@
             <tr>
               <th>Order #</th>
               <th>Item Name</th>
+              <th>Customer</th>
               <th>Frame</th>
               <th>Shipping</th>
               <th>Status</th>
               <th>Date Ordered</th>
             </tr>
             <?php
-              $orderQuery = "SELECT FramedOrders.orderID, FramedProducts.productID, FramedProducts.name, frame, imageURL, shippingMethod, status, timestamp
+              $orderQuery = "SELECT FramedOrders.orderID, CONCAT(FramedUsers.firstName, ' ', FramedUsers.lastName) AS custName, FramedProducts.productID, FramedProducts.name, frame, shippingMethod, status, timestamp
                              FROM FramedOrders JOIN FramedOrderItems ON FramedOrders.orderID = FramedOrderItems.orderID
                                           LEFT JOIN FramedProducts ON FramedProducts.productID = FramedOrderItems.productID
+                                               JOIN FramedUsers ON FramedOrders.userID = FramedUsers.userID
                              WHERE status = 'Processing';";
               $results = mysqli_query($connection, $orderQuery);
               if($results) {
@@ -56,6 +58,7 @@
                     <tr>
                      <td>$orderID</td>
                      <td><a href="{$_ENV["SERVER_ROOT"]}/item/?id={$row['productID']}">{$row['name']}</a></td>
+                     <td>{$row['custName']}</td>
                      <td>{$row['frame']}</td>
                      <td>{$row['shippingMethod']}</td>
                      <td>{$row['status']}</td>
@@ -65,6 +68,39 @@ HERE;
                   $last = $row['orderID'];
                 }
                 mysqli_free_result($results);
+              }
+            ?>
+          </table>
+        </div>
+        <br>
+        <div class="card">
+          <div class="card-header text-white bg-info">
+            Contact Form
+          </div>
+          <table class="table">
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th>Date</th>
+            </tr>
+            <?php
+              $contactQuery = "SELECT name, email, message, timestamp FROM FramedContactForm;";
+              $messages = mysqli_query($connection, $contactQuery);
+
+              if($messages) {
+                while($row = mysqli_fetch_assoc($messages)) {
+                  $dateString = date('M j, Y g:i A', strtotime($row['timestamp']));
+                  echo <<<HERE
+                    <tr>
+                      <td>{$row['name']}</td>
+                      <td>{$row['email']}</td>
+                      <td>{$row['message']}</td>
+                      <td>{$dateString}</td>
+                    </tr>
+HERE;
+                }
+                mysqli_free_result($messages);
               }
               mysqli_close($connection);
             ?>
